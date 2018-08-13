473f0076f4e23c27522e9842aea3e77c67805d59
liuchuanzheng <liuchuanzheng@feiliu.com>
2018-08-13 11:43:53 +0800

    drawerlayout使用

--- app/src/main/java/com/liuchuanzheng/qiekenao/mvp/v/MainActivity.java
+++ app/src/main/java/com/liuchuanzheng/qiekenao/mvp/v/MainActivity.java
@@ -1,15 +1,32 @@
 package com.liuchuanzheng.qiekenao.mvp.v;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.liuchuanzheng.qiekenao.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        DrawerLayout drawerLayout  = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawers();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
