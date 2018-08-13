d1397954563d543f0b4077ad4f52cb58d8a2baed
liuchuanzheng <liuchuanzheng@feiliu.com>
2018-08-13 16:11:41 +0800

    magicindicator

--- app/src/main/java/com/liuchuanzheng/qiekenao/mvp/v/MainActivity.java
+++ app/src/main/java/com/liuchuanzheng/qiekenao/mvp/v/MainActivity.java
@@ -1,21 +1,55 @@
 package com.liuchuanzheng.qiekenao.mvp.v;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.liuchuanzheng.qiekenao.R;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.BezierPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.CommonPagerTitleView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    public List<String> mTitleDataList;
+    private final int[] normalResId = new int[]{
+            R.drawable.ic_explore_normal,
+            R.drawable.ic_live_normal,
+            R.drawable.ic_user_normal
+    };
+    private final int[] pressedResId = new int[]{
+            R.drawable.ic_explore_pressed,
+            R.drawable.ic_live_pressed,
+            R.drawable.ic_user_pressed
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
         initView();
+    }
+
+    private void initData() {
+        mTitleDataList = new ArrayList<>();
+        mTitleDataList.add("主页");
+        mTitleDataList.add("视频");
+        mTitleDataList.add("我的");
     }
 
     private void initView() {
@@ -23,6 +57,65 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout.closeDrawers();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        final MagicIndicator magicIndicator = (MagicIndicator) findViewById(R.id.magic_indicator);
+        CommonNavigator commonNavigator = new CommonNavigator(this);
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+
+            @Override
+            public int getCount() {
+
+                return mTitleDataList == null ? 0 : mTitleDataList.size();
+            }
+
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                CommonPagerTitleView titleView = new CommonPagerTitleView(context);
+                titleView.setContentView(R.layout.item_main_tab_indicator_layout);//加载自定义布局作为Tab
+
+                final Button tab_btn = (Button) titleView.findViewById(R.id.tab_btn);
+                TextView tv_title = titleView.findViewById(R.id.tv_title);
+                tv_title.setText(mTitleDataList.get(index));
+                titleView.setOnPagerTitleChangeListener(new CommonPagerTitleView.OnPagerTitleChangeListener() {
+                    @Override
+                    public void onSelected(int i, int i1) {
+                        tab_btn.setBackgroundResource(pressedResId[i]);
+                    }
+
+                    @Override
+                    public void onDeselected(int i, int i1) {
+                        tab_btn.setBackgroundResource(normalResId[i]);
+                    }
+
+                    @Override
+                    public void onLeave(int i, int i1, float v, boolean b) {
+
+                    }
+
+                    @Override
+                    public void onEnter(int i, int i1, float v, boolean b) {
+
+                    }
+                });
+                titleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        magicIndicator.onPageSelected(index);
+                        magicIndicator.onPageScrolled(index, 0.0F, 0);
+                    }
+                });
+
+                return titleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                BezierPagerIndicator indicator = new BezierPagerIndicator(context);
+                indicator.setColors(Color.RED);
+                return indicator;
+            }
+        });
+        magicIndicator.setNavigator(commonNavigator);
     }
 
     @Override
